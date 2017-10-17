873737fb7cd066efcf142c33b01c2e225277ec1a
Emma Muir <emma_muir@slc.co.uk>
2017-10-17 08:51:08 +0100

    Emma - added if condition function

--- src/learn_functions/functions.clj
+++ src/learn_functions/functions.clj
@@ -7,3 +7,20 @@
 
 (map #(apply max %) [[1 2 3][4 5 6][7 8 9]])
 
+;; if condition
+(def status "help")
+
+;; checking for null value
+(if (nil? status)
+  (prn "your status is nil")
+  (prn (str "your status is not nil")))
+
+;; you can remove nil?
+(if status
+  (prn (str "your status is " status))
+  (prn "you do not have any status"))
+
+(def age 5)
+(if (< age 8)
+  (prn "you are younger than me")
+  (prn "you are older than me"))
