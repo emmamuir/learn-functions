c3e2df0e82556f26147ccfd0d64493ef30541aa9
Emma Muir <emma_muir@slc.co.uk>
2017-10-18 13:50:50 +0100

    Emma - added reduce function examples

--- src/learn_functions/functions2.clj
+++ src/learn_functions/functions2.clj
@@ -1,3 +1,14 @@
 (ns learn-functions.functions2
   (:require [clojure.test :refer :all]))
 
+;; reduce
+(reduce + [1 2 3 4 5])
+
+;; vector to a set
+(reduce conj #{} [:a :b :c])
+
+(reduce conj [1 2 3] [4 5 6])
+
+(reduce * '(1 2 3 4))
+
+(reduce +  [1 2 3 4])
