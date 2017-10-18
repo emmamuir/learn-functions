db9b610e31e9525a1e9aa80d25674c90f09e0d1c
Emma Muir <emma_muir@slc.co.uk>
2017-10-18 14:01:48 +0100

    Emma - added more reduce function examples

--- src/learn_functions/functions2.clj
+++ src/learn_functions/functions2.clj
@@ -4,11 +4,23 @@
 ;; reduce
 (reduce + [1 2 3 4 5])
 
+(reduce * '(1 2 3 4))
+
 ;; vector to a set
 (reduce conj #{} [:a :b :c])
 
 (reduce conj [1 2 3] [4 5 6])
 
-(reduce * '(1 2 3 4))
+;; return largest value
+(reduce (fn [a b]
+            (if (> a b)
+              a
+              b))
+              0
+        [1 2 3 4 5])
 
-(reduce +  [1 2 3 4])
+;; general format
+;(reduce (fn [left right]
+;            (dosomething left right))
+;        starting-value
+;        collection)
