6f07b52920a6c87333f16746fe739d2567ebb4c3
Emma Muir <emma_muir@slc.co.uk>
2017-10-24 13:07:10 +0100

    Emma - refactored destructuring map example

--- src/learn_functions/try_things_2.clj
+++ src/learn_functions/try_things_2.clj
@@ -1,6 +1,16 @@
 (ns learn-functions.try-things-2)
 
-(def person {:name "Emma" :age 21 :address {:number 15 :line1 "Hollow Avenue" :line2 "Renfrew" :town "Paisley"}})
+(def person {:name "Emma"
+             :age 21
+             :address
+                   {:number 15
+                    :line1 "Hollow Avenue"
+                    :line2 "Renfrew"
+                    :town "Paisley"}})
 
+(let [{name :name age :age {:keys [:number]} :address} person]
+  (prn number))
+
+; same as
 (let [{name :name age :age {number :number} :address} person]
   (prn number))
